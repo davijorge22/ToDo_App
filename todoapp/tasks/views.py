90a768dee2192dcbb1da873cfad46897b8d7263e
davijorge22 <davi.jpontes@gmail.com>
2023-03-02 16:46:58 -0300

    need to finish the template

--- todoapp/tasks/views.py
+++ todoapp/tasks/views.py
@@ -26,5 +26,25 @@ def list(request):
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
 
-    context = {"task": task}
-    return render(request, 'update_task.html')
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect("/")
+    context = {"form": form}
+    return render(request, 'update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect("/")
+
+    context = {"item": item}
+    return render(request, 'delete_task.html', context)
